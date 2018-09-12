2acd768eacf904b137bad2559ee672170574add6
alstof <alstof@gmail.com>
2018-09-11 17:04:28 -0700

    Finish ch.2

--- arrMap/challenges-07.test.js
+++ arrMap/challenges-07.test.js
@@ -30,6 +30,11 @@ const forLoopTwoToThe = input => {
 
 const forEachTwoToThe = input => {
   // Solution code here...
+  let arrResult = [];
+  input.forEach(elm => {
+    arrResult.push(2 ** elm);
+  });
+  return arrResult;
 };
 
 // ------------------------------------------------------------------------------------------------
@@ -187,7 +192,7 @@ describe("Testing challenge 1", () => {
   });
 });
 
-xdescribe("Testing challenge 2", () => {
+describe("Testing challenge 2", () => {
   test("It should return two raised to the power of the integer", () => {
     expect(forEachTwoToThe([0, 4, 5])).toStrictEqual([1, 16, 32]);
     expect(forEachTwoToThe([0, 4, 5]).length).toStrictEqual(3);
